5d08ec045329e3a59167133bd50f1e929298ca7b
HunteRoi <me@tinaeldevresse.eu>
2024-06-13 20:06:29 +0200

    tests(fileSink): verify fs calls and simplfy fileSink

--- src/fileSink.ts
+++ src/fileSink.ts
@@ -1,7 +1,7 @@
-import {LogEvent, LogEventLevel} from "./logEvent";
-import {Sink} from "./sink";
-import * as fsp from "fs/promises";
-import * as fs from "fs";
+import * as fsp from 'fs/promises';
+
+import { LogEvent, LogEventLevel } from './logEvent';
+import { Sink } from './sink';
 
 export enum FileSize {
   ONE_KB = 1024,
@@ -16,98 +16,35 @@ export interface FileSinkOptions {
   maxFileSize?: number;
 }
 
-class FileSink implements Sink {
-  name: string;
-  newFile: boolean;
-  readonly level: LogEventLevel;
-  readonly outputDir: string;
-  readonly maxFileSize: number;
-  readonly content: string[];
+export const defaultFileSinkOptions: FileSinkOptions = {
+  outputDir: './logs',
+  logEventLevel: LogEventLevel.verbose,
+  maxFileSize: FileSize.ONE_MB * 10,
+};
 
-  constructor({ fileName, outputDir, logEventLevel, maxFileSize }: FileSinkOptions) {
-    this.name = fileName || new Date().toISOString().split("T")[0];
-    this.level = logEventLevel || LogEventLevel.verbose;
-    this.outputDir = outputDir || "./logs";
-    this.maxFileSize = maxFileSize || FileSize.ONE_MB * 10;
+export class FileSink implements Sink {
+  protected options: FileSinkOptions;
+  protected name: string;
+  private content: string[];
+  private fileSystem: typeof fsp;
+
+  constructor(options: FileSinkOptions, fileSystem = fsp) {
+    this.options = {
+      ...defaultFileSinkOptions,
+      ...(options || {})
+    };
+    this.name = this.options.fileName || new Date().toISOString().split('T')[0];
     this.content = [];
-    this.newFile = true;
-  }
-  get fullname(): string {
-    const extension = LogEventLevel[this.level];
-    return `${this.name}-${extension}`;
+    this.fileSystem = fileSystem;
   }
 
-  get filePath(): string {
-    return `${this.outputDir}/${this.fullname}.log`;
-  }
-
-  /**
-   * Check if the file exists and if it's size is greater than the maxFileSize
-   * If the file is too big, we roll it
-   */
-  async manageFiles(): Promise<void> {
-    this.changeFileNameIfDateChanged();
-    await this.checkFileExists();
-    const fileStats = await fsp.stat(this.filePath);
-
-    if (fileStats.size > this.maxFileSize) {
-      await this.rollFile();
-    }
-  }
-
-  countTodaysLogFiles(files: string[]): number {
-    return files.filter(name => name.includes(this.fullname)).length;
-  }
-
-  /**
-   * Roll the file by renaming it to include the number of files created today
-   */
-  async rollFile(): Promise<void> {
-    try {
-      const files = await fsp.readdir(this.outputDir);
-      const todayFilesNumber = this.countTodaysLogFiles(files);
-
-      await fsp.rename(this.filePath, `${this.filePath}.${todayFilesNumber}`);
-    } catch (_error) {
-      // File already renamed, we don't care
-    } finally {
-      await this.checkFileExists();
-    }
-  }
-
-  /**
-   * Check if the file exists and set the newFile flag accordingly
-   */
-  async checkFileExists() {
-    try {
-      await fsp.access(this.filePath, fs.constants.F_OK);
-      this.newFile = false;
-    } catch (_error) {
-      await fsp.writeFile(this.filePath, "", { encoding: "utf-8" });
-      this.newFile = true;
-    }
-  }
-
-  /**
-   * If the file name is a date and if it's different from today's date, we change it
-   */
-  changeFileNameIfDateChanged() {
-    if (/\d{4}-\d{2}-\d{2}/.test(this.name)) {
-      const [potentialNewFileName] = new Date().toISOString().split("T");
-
-      if (potentialNewFileName !== this.name) {
-        this.name = potentialNewFileName;
-      }
-    }
-  }
-
-  async emit(events: LogEvent[]): Promise<void> {
-    if (this.level === LogEventLevel.off) return;
+  public async emit(events: LogEvent[]) {
+    if (this.options.logEventLevel === LogEventLevel.off) return;
 
     await this.manageFiles();
 
     for (const event of events) {
-      if (this.level === LogEventLevel.verbose || this.level === event.level) {
+      if (this.options.logEventLevel === LogEventLevel.verbose || this.options.logEventLevel === event.level) {
         this.content.push(`[${LogEventLevel[event.level]}] ${event.messageTemplate.render()}`);
       }
     }
@@ -115,11 +52,73 @@ class FileSink implements Sink {
     await this.flush();
   }
 
-  async flush(): Promise<void> {
-    await fsp.appendFile(this.filePath, `${this.newFile ? "" : "\n"}${this.content.join("\n")}`, {
-      encoding: "utf-8"
-    });
+  public async flush() {
+    if (this.content.length > 0) {
+      await this.fileSystem.appendFile(this.filePath, this.content.join('\n'), {
+        encoding: 'utf-8'
+      });
 
-    this.content.length = 0;
+      this.content.length = 0;
+    }
+  }
+
+  private get filePath(): string {
+    return `${this.options.outputDir}/${this.name}.log`;
+  }
+
+  /**
+   * Check if the file exists and if it's size is greater than the maxFileSize
+   * If the file is too big, we roll it
+   */
+  private async manageFiles() {
+    this.changeFileNameIfDateChanged();
+    await this.checkFileExists();
+    const fileStats = await this.fileSystem.stat(this.filePath);
+
+    if (fileStats.size >= this.options.maxFileSize) {
+      await this.rollFile();
+    }
+  }
+
+  /**
+   * Check if the file exists and set the newFile flag accordingly
+   */
+  private async checkFileExists() {
+    try {
+      await this.fileSystem.access(this.filePath, fsp.constants.F_OK);
+      this.content.push('\n');
+    } catch (_error) {
+      await this.fileSystem.writeFile(this.filePath, '', { encoding: 'utf-8' });
+    }
+  }
+
+  /**
+   * Roll the file by renaming it to include the number of files created today
+   *
+   * @remarks The file with no number is the most recent one, then followed by files in descending order.
+  */
+  private async rollFile() {
+    try {
+      const outputFiles = await this.fileSystem.readdir(this.options.outputDir);
+      const todayFilesNumber = outputFiles.filter(filename => filename.includes(this.name)).length;
+
+      await this.fileSystem.rename(this.filePath, `${this.filePath}.${todayFilesNumber}`);
+    } catch (_error) {
+      // File already renamed
+    } finally {
+      await this.checkFileExists();
+    }
+  }
+
+  /**
+   * If the file name is a date and if it's different from today's date, we change it
+   */
+  private changeFileNameIfDateChanged() {
+    if (/\d{4}-\d{2}-\d{2}/.test(this.name)) {
+      const potentialNewFileName = new Date().toISOString().split('T')[0];
+      if (potentialNewFileName !== this.name) {
+        this.name = potentialNewFileName;
+      }
+    }
   }
 }

4cddaada6c57903bd5373622d048eac25eb18303
HunteRoi <me@tinaeldevresse.eu>
2024-06-26 15:32:36 +0200

    tests(fileSink): refactor to check each fsp call separately

--- test/fileSink.spec.ts
+++ test/fileSink.spec.ts
@@ -11,19 +11,26 @@ import { MessageTemplate } from '../src/messageTemplate';
 import { FileSink } from '../src/fileSink';
 
 describe('FileSink', () => {
-    const fileSystemMock = TypeMoq.Mock.ofType<typeof fsp>();
+    let fileSystemMock: TypeMoq.IMock<typeof fsp>;
+
+    beforeEach(() => {
+        fileSystemMock = TypeMoq.Mock.ofType<typeof fsp>();
+    });
 
     describe('emit()', () => {
         it('creates a file if it does not exist', async () => {
             fileSystemMock.setup(fs => fs.access(TypeMoq.It.isAnyString(), TypeMoq.It.isAnyNumber())).throws(new Error('file does not exist'));
-            fileSystemMock.setup(fs => fs.writeFile(TypeMoq.It.isAnyString(), TypeMoq.It.isAnyString())).returns(() => Promise.resolve());
+            fileSystemMock.setup(fs => fs.writeFile(TypeMoq.It.isAnyString(), TypeMoq.It.isAnyString(), TypeMoq.It.isAny())).returns(() => Promise.resolve());
             fileSystemMock.setup(fs => fs.stat(TypeMoq.It.isAnyString())).returns(() => Promise.resolve({ size: 0 } as Stats));
             fileSystemMock.setup(fs => fs.appendFile(TypeMoq.It.isAnyString(), TypeMoq.It.isAnyString(), TypeMoq.It.isAny())).returns(() => Promise.resolve());
             const sink = new FileSink({}, fileSystemMock.object);
 
             await sink.emit([new LogEvent('', LogEventLevel.information, new MessageTemplate('test'))]);
 
-            fileSystemMock.verifyAll();
+            fileSystemMock.verify(fs => fs.access(TypeMoq.It.isAnyString(), TypeMoq.It.isAnyNumber()), TypeMoq.Times.once());
+            fileSystemMock.verify(fs => fs.writeFile(TypeMoq.It.isAnyString(), TypeMoq.It.isAnyString(), TypeMoq.It.isAny()), TypeMoq.Times.once());
+            fileSystemMock.verify(fs => fs.stat(TypeMoq.It.isAnyString()), TypeMoq.Times.once());
+            fileSystemMock.verify(fs => fs.appendFile(TypeMoq.It.isAnyString(), '[information] test', TypeMoq.It.isAny()), TypeMoq.Times.once());
         });
 
         it('rolls the file if it exceeds the max file size', () => {
@@ -32,13 +39,18 @@ describe('FileSink', () => {
             fileSystemMock.setup(fs => fs.readdir(TypeMoq.It.isAnyString())).returns(() => Promise.resolve(['maxSizeFile.log']));
             fileSystemMock.setup(fs => fs.rename(TypeMoq.It.isAnyString(), TypeMoq.It.isAnyString())).returns(() => Promise.resolve());
             fileSystemMock.setup(fs => fs.access(TypeMoq.It.isAnyString(), TypeMoq.It.isAnyNumber())).throws(new Error('file does not exist'));
-            fileSystemMock.setup(fs => fs.writeFile(TypeMoq.It.isAnyString(), TypeMoq.It.isAnyString())).returns(() => Promise.resolve());
+            fileSystemMock.setup(fs => fs.writeFile(TypeMoq.It.isAnyString(), TypeMoq.It.isAnyString(), TypeMoq.It.isAny())).returns(() => Promise.resolve());
             fileSystemMock.setup(fs => fs.appendFile(TypeMoq.It.isAnyString(), TypeMoq.It.isAnyString(), TypeMoq.It.isAny())).returns(() => Promise.resolve());
             const sink = new FileSink({ maxFileSize: 1024 }, fileSystemMock.object);
 
             sink.emit([new LogEvent('', LogEventLevel.information, new MessageTemplate('test'))]);
 
-            fileSystemMock.verifyAll();
+            fileSystemMock.verify(fs => fs.access(TypeMoq.It.isAnyString(), TypeMoq.It.isAnyNumber()), TypeMoq.Times.exactly(2));
+            fileSystemMock.verify(fs => fs.stat(TypeMoq.It.isAnyString()), TypeMoq.Times.once());
+            fileSystemMock.verify(fs => fs.readdir(TypeMoq.It.isAnyString()), TypeMoq.Times.once());
+            fileSystemMock.verify(fs => fs.rename(TypeMoq.It.isAnyString(), TypeMoq.It.isAnyString()), TypeMoq.Times.once());
+            fileSystemMock.verify(fs => fs.writeFile(TypeMoq.It.isAnyString(), TypeMoq.It.isAnyString(), TypeMoq.It.isAny()), TypeMoq.Times.once());
+            fileSystemMock.verify(fs => fs.appendFile(TypeMoq.It.isAnyString(), '[information] test', TypeMoq.It.isAny()), TypeMoq.Times.once());
         });
 
         it('appends to existing file', () => {
@@ -49,7 +61,9 @@ describe('FileSink', () => {
 
             sink.emit([new LogEvent('', LogEventLevel.information, new MessageTemplate('test'))]);
 
-            fileSystemMock.verifyAll();
+            fileSystemMock.verify(fs => fs.access(TypeMoq.It.isAnyString(), TypeMoq.It.isAnyNumber()), TypeMoq.Times.once());
+            fileSystemMock.verify(fs => fs.stat(TypeMoq.It.isAnyString()), TypeMoq.Times.once());
+            fileSystemMock.verify(fs => fs.appendFile(TypeMoq.It.isAnyString(), '[information] test', TypeMoq.It.isAny()), TypeMoq.Times.once());
         });
 
         it('does not log events lower than the restricted level', () => {
@@ -60,7 +74,9 @@ describe('FileSink', () => {
 
             sink.emit([new LogEvent('', LogEventLevel.information, new MessageTemplate('test'))]);
 
-            fileSystemMock.verifyAll();
+            fileSystemMock.verify(fs => fs.access(TypeMoq.It.isAnyString(), TypeMoq.It.isAnyNumber()), TypeMoq.Times.once());
+            fileSystemMock.verify(fs => fs.stat(TypeMoq.It.isAnyString()), TypeMoq.Times.once());
+            fileSystemMock.verify(fs => fs.appendFile(TypeMoq.It.isAnyString(), '[information] test', TypeMoq.It.isAny()), TypeMoq.Times.never());
         });
     });
 });
